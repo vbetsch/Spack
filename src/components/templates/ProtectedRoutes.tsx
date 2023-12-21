018581902c3eea19f53a2a97a98165924f8bf166
vbetsch <vic.betsch@gmail.com>
2023-12-21 01:01:56 +0100

    :recycle: Use getUser query

--- src/components/templates/ProtectedRoutes.tsx
+++ src/components/templates/ProtectedRoutes.tsx
@@ -2,39 +2,33 @@ import { useContext, useEffect } from "react";
 import type { ReactNode } from "react";
 import { AuthContext } from "../../providers/AuthProvider.tsx";
 import { Navigate, useLocation } from "react-router";
-import { AuthActionEnum } from "../../reducers/AuthReducer.ts";
 import { Outlet } from "react-router-dom";
-import { doc, getDoc } from "firebase/firestore";
-import { db } from "../../database/firebase.ts";
-import type { AuthUser } from "../../types/AuthUserType.ts";
-import { DatabaseCollectionEnum } from "../../types/DatabaseCollectionEnum.ts";
+import { getUser } from "../../database/queries/UserQueries.ts";
+import { UserDocument } from "../../types/documents/UserDocument.ts";
+import { AuthActionEnum } from "../../reducers/AuthReducer.ts";
 
 export const ProtectedRoutes = (): ReactNode => {
-    const { dispatch } = useContext(AuthContext);
-    const location = useLocation();
     const user = localStorage.getItem("@user");
-
-    const getUser = async (): Promise<void> => {
-        if (user != null) {
-            const userData: AuthUser = JSON.parse(user);
-            const userSnap = await getDoc(
-                doc(db, DatabaseCollectionEnum.USERS, userData.uid.toString()),
-            );
-            dispatch({
-                type: AuthActionEnum.LOGIN,
-                payload: {
-                    id: userSnap.id,
-                    ...userSnap.data(),
-                },
-            });
-        }
-    };
+    const { state, dispatch } = useContext(AuthContext);
+    const location = useLocation();
 
     useEffect(() => {
-        getUser().catch((e) => {
-            console.error(e);
-        });
-    }, [user]);
+        getUser()
+            .then((snap) => {
+                if (snap != null) {
+                    dispatch({
+                        type: AuthActionEnum.LOGIN,
+                        payload: {
+                            id: snap.id,
+                            ...snap.data(),
+                        } as UserDocument,
+                    });
+                }
+            })
+            .catch((e) => {
+                console.error(e);
+            });
+    }, [state.currentUser]);
 
     if (user == null) {
         console.warn(
